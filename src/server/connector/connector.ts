a6259375bc6bc09c2f4ca0347b06987d3352aea2
Lasse Sprengel <lasse.sprengel@gmail.com>
2017-01-29 02:31:54 +0100

    Moved to master and fixed socket error Fixed #2 and resolved #3

--- src/server/connector/connector.ts
+++ src/server/connector/connector.ts
@@ -1,31 +1,78 @@
-import * as tls from 'tls';
+import { connect, createServer, ConnectionOptions, TlsOptions } from 'tls';
 import * as winston from 'winston';
 
 import { Cluster } from 'cluster';
 
 export class Connector {
+
+    private socketOptions: ConnectionOptions;
+    private serverOptions: TlsOptions;
+
     constructor(private instances: Number, key: string, cert: string, serverCerts: string[]) {
-        let options: tls.ConnectionOptions = {
+        this.socketOptions = {
             key,
             cert,
 
-            
             ca: serverCerts
         };
 
-        let socket = tls.connect(8000, options, () => {
+        this.serverOptions = {
+            key,
+            cert,
+
+            requestCert: true,
+            rejectUnauthorized: true,
+            ca: serverCerts
+        }
+    }
+
+    public Start(port: number, authPort: number, host?: string) {
+        winston.info("-------------------------");
+        winston.info("Connecting to auth-server");
+        this.ConnectAuthServer(authPort, host);
+        winston.info("Starting connector server");
+        this.StartConnectorServer(port);
+        winston.info("-------------------------");
+    }
+
+    private StartConnectorServer(port: number) {
+        winston.info("TLS server starting");
+        let server = createServer(this.serverOptions, (stream) => {
+            winston.info(`Stream opened on ${stream.remoteAddress}:${stream.remotePort}`);
+            stream.on("data", (chunk) => {
+                try {
+                    if (typeof chunk === "string") winston.info(chunk);
+                    else winston.info(chunk.toString("utf-8"));
+                } catch (e) {
+                    winston.error(e);
+                }
+            })
+        })
+
+        server.listen(port, () => {
+            winston.info('Server bound on port ' + port);
+        });
+    }
+
+    private ConnectAuthServer(port: number, host?: string) {
+        let socket = connect(port, host, this.socketOptions, () => {
             winston.info('client connected',
                 socket.authorized ? 'authorized' : 'unauthorized');
-            process.stdin.pipe(socket);
-            process.stdin.resume();
         });
         socket.setEncoding('utf8');
         socket.on('data', (data: string) => {
             winston.info(data);
         });
         socket.on('end', () => {
-            winston.info("Connection closed");
+            winston.warn("Connection to auth-server closed");
         });
-        
+        socket.on("error", (e) => {
+            winston.error("Auth server error", e);
+            setTimeout(() => {
+                socket.end();
+                winston.info("Trying to reconnect");
+                this.ConnectAuthServer(port, host);
+            }, 15 * 1000);
+        })
     }
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -10,26 +10,17 @@ export class Server {
     private connector: Connector;
 
     constructor(instances?: Number) {
-        if (cluster.isMaster) {
-            this.maxInstances = instances || process.env.instances || this.maxInstances;
+        this.maxInstances = instances || process.env.instances || this.maxInstances;
 
-            this.CheckConfiguration();
-        }
+        this.CheckConfiguration();
     }
 
-    public Start(key: string, cert: string, serverCerts: string[]) {
-        if (cluster.isMaster) {
-           let connector = cluster.fork();
-           connector.on("exit", (code, signal) => {
-                winston.warn(`Connector ${connector.process.pid} has exited. Trying to restart`);
-                cluster.fork();
-           });
-        } else {
-            this.connector = new Connector(this.maxInstances, key, cert, serverCerts);
-        }
+    public Start(key: string, cert: string, serverCerts: string[], port: number = 3000, authPort: number = 8000) {
+        this.connector = new Connector(this.maxInstances, key, cert, serverCerts);
+        this.connector.Start(port, authPort);
     }
 
-    private CheckConfiguration():void {
+    private CheckConfiguration(): void {
         if (this.maxInstances > numCPUs) winston.warn(`You have configured ${this.maxInstances} instances on a ${numCPUs} core machine. To many threads may reduce performance.`)
         if (this.maxInstances <= 1) throw new Error("You need at least 2 instances.");
     }
